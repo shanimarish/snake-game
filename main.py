4688778449d20fc49642e5180a167dfad263cc5c
Shani <74494158+shanimarish@users.noreply.github.com>
2024-12-01 10:35:07 +0800

    Create snake class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# 1. Create a snake body
-
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -9,30 +8,14 @@ screen.bgcolor("black")
 screen.title("Classic Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    snake_segment = Turtle(shape="square")
-    snake_segment.color("white")
-    snake_segment.up()
-    snake_segment.goto(position)
-    segments.append(snake_segment)
-
-# 2. Move the snake
+snake = Snake()
 
 game_is_on = True
-
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 # 3. Control the snake
 # 4. Detect collision with food
